e6216a4cb6a77f00c4b4ba9cd4c1c60be1a1fe2c
Wojciech Kasprzyk <w.kasprzyk95@gmail.com>
2023-06-23 18:29:06 +0200

    feat(dms): init primeng

--- libs/departments/src/lib/departments.module.ts
+++ libs/departments/src/lib/departments.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DepartmentsComponent } from './departments/departments.component';
 import { SharedStatesDepartmentsModule } from "@rite-nrg-workspace/shared/states/departments";
+import { TableModule } from "primeng/table";
+import { ButtonModule } from "primeng/button";
+import { DialogModule } from "primeng/dialog";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+const PRIME_NG_COMPONENTS = [
+  TableModule,
+  ButtonModule,
+  DialogModule,
+]
 
 @NgModule({
   imports: [
     CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    ...PRIME_NG_COMPONENTS,
     SharedStatesDepartmentsModule
   ],
   declarations: [DepartmentsComponent],
--- libs/users/src/lib/users.module.ts
+++ libs/users/src/lib/users.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users/users.component';
 import { SharedStatesUsersModule } from "@rite-nrg-workspace/shared/states/users";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from "@angular/platform-browser";
+import { TableModule } from "primeng/table";
+import { ButtonModule } from "primeng/button";
+import { DialogModule } from "primeng/dialog";
+
+const PRIME_NG_COMPONENTS = [
+  TableModule,
+  ButtonModule,
+  DialogModule,
+]
 
 @NgModule({
   imports: [
     CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    ...PRIME_NG_COMPONENTS,
     SharedStatesUsersModule
   ],
   declarations: [UsersComponent],
