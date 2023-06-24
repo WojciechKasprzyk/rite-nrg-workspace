e59a08cb95c2a9c2c5e7714007519287d69b3962
Wojciech Kasprzyk <w.kasprzyk95@gmail.com>
2023-06-24 11:12:00 +0200

    feat(shared-ui): implement nrg-table

--- libs/shared/ui/src/lib/components/table/table.component.ts
+++ libs/shared/ui/src/lib/components/table/table.component.ts
@@ -1,12 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from "primeng/button";
+import { SharedModule } from "primeng/api";
+import { TableModule } from "primeng/table";
+
+type TableEntry = {id: number} & Record<string, string>
 
 @Component({
   selector: 'nrg-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule, SharedModule, TableModule],
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TableComponent {}
+export class TableComponent {
+  @Input({required: true}) data!: TableEntry[];
+  @Input({required: true}) loading!: boolean;
+  @Input() selected: TableEntry | undefined;
+  @Input() displayedColumns: string[] = [];
+  @Input() fallbackMessage: string = 'No entries found.';
+
+  @Output() entryDelete = new EventEmitter<number>();
+  @Output() selectionChange = new EventEmitter<TableEntry>();
+
+  handleSelectionChange(entry: TableEntry) {
+    this.selectionChange.observed && this.selectionChange.emit(entry);
+  }
+
+  deleteDepartment(id: number) {
+    this.entryDelete.observed && this.entryDelete.emit(id);
+  }
+
+}
