83a80288164c4a7b4e8c8cfb71ef022801b9cc2e
Wojciech Kasprzyk <w.kasprzyk95@gmail.com>
2023-06-24 11:51:26 +0200

    feat(shared-ui): implement dialog component

--- libs/shared/ui/src/lib/components/dialog/dialog.component.ts
+++ libs/shared/ui/src/lib/components/dialog/dialog.component.ts
@@ -1,12 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DialogModule } from "primeng/dialog";
+import { ButtonModule } from "primeng/button";
 
 @Component({
   selector: 'nrg-dialog',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DialogModule, ButtonModule],
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DialogComponent {}
+export class DialogComponent {
+  @Input() buttonLabel: string = 'Show Dialog';
+  @Input() header: string = 'Header';
+  @Output() confirm = new EventEmitter();
+  visible = false;
+
+  showDialog() {
+    this.visible = true;
+  }
+
+  handleConfirm() {
+    this.visible = false;
+    this.confirm.observed && this.confirm.emit();
+  }
+}
