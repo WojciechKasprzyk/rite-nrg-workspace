c6109fce3c9764268b6d4f8ccb787d73aeff5ef8
Wojciech Kasprzyk <w.kasprzyk95@gmail.com>
2023-06-27 16:15:07 +0200

    test(users): fix test configuration

--- libs/users/src/lib/users/services/user-form/user-form.service.spec.ts
+++ libs/users/src/lib/users/services/user-form/user-form.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserFormService } from './user-form.service';
+import { MockProvider } from "ng-mocks";
+import { DepartmentsFacade } from "@rite-nrg-workspace/shared/states/departments";
+import { of } from "rxjs";
 
 describe('UserFormService', () => {
   let service: UserFormService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        UserFormService,
+        MockProvider(DepartmentsFacade, {
+          selectedDepartment$: of()
+        })
+      ]
+    });
     service = TestBed.inject(UserFormService);
   });
 
