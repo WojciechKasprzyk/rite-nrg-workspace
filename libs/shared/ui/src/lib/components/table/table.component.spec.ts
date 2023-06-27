ed9f59869926c3e2fabe1c5c0b4af71c21a305bb
Wojciech Kasprzyk <w.kasprzyk95@gmail.com>
2023-06-27 09:48:50 +0200

    test(shared-ui): add shared-ui tests

--- libs/shared/ui/src/lib/components/table/table.component.spec.ts
+++ libs/shared/ui/src/lib/components/table/table.component.spec.ts
@@ -1,9 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TableComponent } from './table.component';
+import { Entry } from "@rite-nrg-workspace/shared/api";
 
 describe('TableComponent', () => {
-  let component: TableComponent;
-  let fixture: ComponentFixture<TableComponent>;
+  let component: TableComponent<Entry>;
+  let fixture: ComponentFixture<TableComponent<Entry>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,10 +13,19 @@ describe('TableComponent', () => {
 
     fixture = TestBed.createComponent(TableComponent);
     component = fixture.componentInstance;
+    setInputs();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  function setInputs() {
+    component.data = [
+      {id: 1},
+      {id: 2},
+    ];
+    component.loading = false;
+  }
 });
--- libs/shared/ui/src/lib/pipes/paginator/paginator.pipe.spec.ts
+++ libs/shared/ui/src/lib/pipes/paginator/paginator.pipe.spec.ts
@@ -1,8 +1,54 @@
 import { PaginatorPipe } from './paginator.pipe';
+import { User } from "@rite-nrg-workspace/shared/api";
+
+const data: User[] = [
+  {
+    "id": 1,
+    "name": "John Doe",
+    "email": "johndoe@company.com"
+  },
+  {
+    "id": 2,
+    "name": "Jane Smith",
+    "email": "janesmith@company.com"
+  },
+  {
+    "id": 3,
+    "name": "Bob Johnson",
+    "email": "bobjohnson@company.com"
+  },
+  {
+    "id": 4,
+    "name": "Alice Brown",
+    "email": "alicebrown@othercompany.com"
+  }
+];
+
+const expected = [
+  {
+    "id": 3,
+    "name": "Bob Johnson",
+    "email": "bobjohnson@company.com"
+  },
+  {
+    "id": 4,
+    "name": "Alice Brown",
+    "email": "alicebrown@othercompany.com"
+  }
+]
 
 describe('PaginatorPipe', () => {
   it('create an instance', () => {
     const pipe = new PaginatorPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should filter list respectively to arguments', () => {
+    const pipe = new PaginatorPipe();
+    const result = pipe.transform(data, {
+      first: 2,
+      rows: 2
+    })
+    expect(result).toStrictEqual(expected);
   });
 });
