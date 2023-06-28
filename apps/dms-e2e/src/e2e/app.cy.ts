c6609d70672dbed86c231876fdd7ac64479d851d
Wojciech Kasprzyk <w.kasprzyk95@gmail.com>
2023-06-28 11:05:50 +0200

    feat(dms-e2e): setup e2e cypress tests

--- apps/dms-e2e/src/e2e/app.cy.ts
+++ apps/dms-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,38 @@
 import { getGreeting } from '../support/app.po';
 
+const headerTexts = [
+  'Departments',
+  'Users within department'
+]
+
 describe('dms', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome dms');
+  it('should display headers', () => {
+    cy.get('body')
+      .find('h2')
+      .should('have.length', 2)
+      .each((h2, i) => {
+        expect(h2.text()).to.eq(headerTexts[i]);
+      })
   });
+
+  it('should display departments', () => {
+    cy.get('nrg-departments')
+      .find('tr')
+      .should('have.length', 3);
+
+    cy.get('nrg-users')
+      .find('tr')
+      .should('have.length', 2);
+
+    cy.get('nrg-departments')
+      .find('tr')
+      .contains('1')
+      .click();
+
+    cy.get('nrg-users')
+      .find('tr')
+      .should('have.length', 3);
+  })
 });
